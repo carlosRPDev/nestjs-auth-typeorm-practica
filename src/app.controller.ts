fc92a4f656946d3c48ff257cf1d0628a21167a70
Carlos Rodriguez <carlos.rodriguez@qcode.co>
2021-11-30 00:35:05 -0500

    Clase 10

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,29 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  UseGuards,
+  SetMetadata,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
 
+import { ApiKeyGuard } from './auth/guards/api-key.guard';
+
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Public()
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('nuevo')
+  @Public()
   newEndpoint() {
     return 'yo soy nuevo';
   }
